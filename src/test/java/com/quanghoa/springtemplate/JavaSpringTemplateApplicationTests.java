e671ac7596f27aa374b694aa25d0cffaf2156761
Vo Quang Hoa <hvquang@openwt.com>
2023-02-17 00:13:46 +0700

    Adding db

--- src/test/java/com/quanghoa/springtemplate/JavaSpringTemplateApplicationTests.java
+++ src/test/java/com/quanghoa/springtemplate/JavaSpringTemplateApplicationTests.java
@@ -1,13 +1,20 @@
 package com.quanghoa.springtemplate;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 
 @SpringBootTest
 class JavaSpringTemplateApplicationTests {
 
+
+    @Autowired
+    private Environment environment;
+
     @Test
     void contextLoads() {
+        System.out.println("###### environment: " + String.join(", ", environment.getActiveProfiles()));
     }
 
 }
