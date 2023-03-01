891355082a9b0b088338e4c87d4043f13d59ad33
Vo Quang Hoa <hvquang@openwt.com>
2023-03-01 22:49:58 +0700

    Add swagger

--- src/main/java/com/quanghoa/springtemplate/api/book/BookController.java
+++ src/main/java/com/quanghoa/springtemplate/api/book/BookController.java
@@ -1,6 +1,7 @@
 package com.quanghoa.springtemplate.api.book;
 
 import com.quanghoa.springtemplate.domain.book.BookService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +15,10 @@ import static com.quanghoa.springtemplate.api.book.BookDTOMapper.toBookDTOs;
 @RequestMapping("/api/v1/books")
 @RequiredArgsConstructor
 public class BookController {
+
     private final BookService bookService;
 
+    @Operation(summary = "Find all available books")
     @GetMapping
     public List<BookDTO> findAll() {
         return toBookDTOs(bookService.findAll());
